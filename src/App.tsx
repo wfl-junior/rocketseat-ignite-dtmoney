525a25a092c15782bbb9e75fb1b522de24cd803a
Wallace Júnior <wflj1997@hotmail.com>
2022-06-25 13:32:47 -0300

    Instalando Styled Components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 64px;
+  color: #8257e6;
+`;
+
 export const App: React.FC = () => (
   <div className="App">
-    <h1>Hello World</h1>
+    <Title>Hello World</Title>
   </div>
 );
