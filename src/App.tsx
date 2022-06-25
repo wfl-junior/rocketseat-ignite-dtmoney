707561c8cf18129e4d79ebfbd3e8ce4c7c922034
Wallace Júnior <wflj1997@hotmail.com>
2022-06-25 15:43:00 -0300

    Configurando modal de criação

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
+import ReactModal from "react-modal";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 
-export const App: React.FC = () => (
-  <Fragment>
-    <GlobalStyle />
+ReactModal.setAppElement("#root");
 
-    <Header />
-    <Dashboard />
-  </Fragment>
-);
+export const App: React.FC = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] =
+    useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
+  return (
+    <Fragment>
+      <GlobalStyle />
+
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+      <Dashboard />
+
+      <ReactModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      >
+        <h2>Cadastrar transação</h2>
+      </ReactModal>
+    </Fragment>
+  );
+};
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,22 @@
 import logoImage from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
-export const Header: React.FC = () => (
-  <Container>
-    <Content>
-      <img src={logoImage} alt="dt money logo" />
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
 
-      <button type="button">Nova transação</button>
-    </Content>
-  </Container>
-);
+export const Header: React.FC<HeaderProps> = ({
+  onOpenNewTransactionModal,
+}) => {
+  return (
+    <Container>
+      <Content>
+        <img src={logoImage} alt="dt money logo" />
+
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
+      </Content>
+    </Container>
+  );
+};
