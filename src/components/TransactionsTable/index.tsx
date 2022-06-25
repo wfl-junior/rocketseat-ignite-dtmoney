d6331f1224fd71d702d947fb3efe36dc6643ca08
Wallace Júnior <wflj1997@hotmail.com>
2022-06-25 15:23:08 -0300

    Configurando MirageJS

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,31 +1,40 @@
+import { useEffect } from "react";
 import { Container } from "./styles";
 
-export const TransactionsTable: React.FC = () => (
-  <Container>
-    <table>
-      <thead>
-        <tr>
-          <th>Título</th>
-          <th>Valor</th>
-          <th>Categoria</th>
-          <th>Data</th>
-        </tr>
-      </thead>
+export const TransactionsTable: React.FC = () => {
+  useEffect(() => {
+    fetch("http://localhost:3000/api/transactions")
+      .then(response => response.json())
+      .then(data => console.log(data));
+  }, []);
 
-      <tbody>
-        <tr>
-          <td>Desenvolvimento de website</td>
-          <td className="deposit">R$12.000</td>
-          <td>Desenvolvimento</td>
-          <td>20/02/2021</td>
-        </tr>
-        <tr>
-          <td>Aluguel</td>
-          <td className="withdraw">- R$1.100</td>
-          <td>Casa</td>
-          <td>17/02/2021</td>
-        </tr>
-      </tbody>
-    </table>
-  </Container>
-);
+  return (
+    <Container>
+      <table>
+        <thead>
+          <tr>
+            <th>Título</th>
+            <th>Valor</th>
+            <th>Categoria</th>
+            <th>Data</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          <tr>
+            <td>Desenvolvimento de website</td>
+            <td className="deposit">R$12.000</td>
+            <td>Desenvolvimento</td>
+            <td>20/02/2021</td>
+          </tr>
+          <tr>
+            <td>Aluguel</td>
+            <td className="withdraw">- R$1.100</td>
+            <td>Casa</td>
+            <td>17/02/2021</td>
+          </tr>
+        </tbody>
+      </table>
+    </Container>
+  );
+};
